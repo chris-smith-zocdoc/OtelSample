1f2f100c04f41e7a0f377a6b4a7752b72d5ce900
chris.smith <chris.smith@zocdoc.com>
2022-06-16 20:49:58 -0400

    Setup working demo for https://github.com/open-telemetry/opentelemetry-dotnet/issues/3286

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,20 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Diagnostics;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Trace;
 
 namespace OtelSample
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private TracerProvider tracerProvider;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var builder = Sdk.CreateTracerProviderBuilder()
+                .AddAspNetInstrumentation()
+                .SetSampler(new AlwaysOnSampler())
+                .AddConsoleExporter(options => options.Targets = ConsoleExporterOutputTargets.Debug);
+            
+            this.tracerProvider = builder.Build();
+        }
+
+        protected void Application_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            var act = Activity.Current; // Activity is ok here
+        }
+
+        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            // But typically null here because its not restored in the HttpModule
+            // https://github.com/open-telemetry/opentelemetry-dotnet/blob/55c5dd18e9bcb8f9f728aa1a0557bd5157c88362/src/OpenTelemetry.Instrumentation.AspNet.TelemetryHttpModule/TelemetryHttpModule.cs#L88-L91
+            var act = Activity.Current; 
         }
     }
 }
