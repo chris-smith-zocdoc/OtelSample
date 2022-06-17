78adf9ec50df61603992e6993cf42a06fbf7d44d
chris.smith <chris.smith@zocdoc.com>
2022-06-16 20:19:09 -0400

    delete unused frontend stuff

--- Global.asax.cs
+++ Global.asax.cs
@@ -15,7 +15,6 @@ namespace OtelSample
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
